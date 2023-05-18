86a1d161cc518407dc178d33c68a3e9ee67f0df1
Vito Alessandro Carella <berlinodeveloper@gmail.com>
2023-05-18 13:41:10 +0200

    update icon and add new icons

---
+++ src/com/plugin/mining/actions/ConformanceCheckingActionController.java
@@ -0,0 +1,20 @@
+package com.plugin.mining.actions;
+
+import com.plugin.mining.logging.Logger;
+import com.vp.plugin.action.VPAction;
+import com.vp.plugin.action.VPActionController;
+
+public class ConformanceCheckingActionController implements VPActionController {
+    private final Logger logger = new Logger(ConformanceCheckingActionController.class);
+
+    @Override
+    public void performAction(VPAction vpAction) {
+        // Empty
+    }
+
+    @Override
+    public void update(VPAction vpAction) {
+        // Empty
+    }
+
+}

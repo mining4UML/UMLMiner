ce0b70302506cd4abd3a3ca2785ede6b2e0aeafc
Vito Alessandro Carella <berlinodeveloper@gmail.com>
2023-05-24 22:36:06 +0200

    some fix export log action Export files di log #16

--- src/com/plugin/mining/actions/ExportLogsActionController.java
+++ src/com/plugin/mining/actions/ExportLogsActionController.java
@@ -3,10 +3,13 @@ package com.plugin.mining.actions;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.TextField;
+import java.awt.TrayIcon.MessageType;
 import java.util.Arrays;
+import java.util.Locale;
 
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import com.plugin.mining.logging.LogStream;
@@ -31,6 +34,7 @@ public class ExportLogsActionController implements VPActionController {
 
     private JFileChooser createFileChooser() {
         JFileChooser fileChooser = viewManager.createJFileChooser();
+        fileChooser.setLocale(Locale.ENGLISH);
         fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         fileChooser.setName(ACTION_NAME);
         fileChooser.setDialogTitle(ACTION_NAME);
@@ -45,14 +49,16 @@ public class ExportLogsActionController implements VPActionController {
     public void performAction(VPAction vpAction) {
 
         if (LogStream.countLogs() == 0) {
-            viewManager.showMessageDialog(viewManager.getRootFrame(), "No logs are found.");
+            viewManager.showMessageDialog(viewManager.getRootFrame(), "No logs found.", ACTION_NAME,
+                    JOptionPane.WARNING_MESSAGE);
             return;
         }
 
         JFileChooser fileChooser = createFileChooser();
         if (fileChooser.showSaveDialog(viewManager.getRootFrame()) == JFileChooser.APPROVE_OPTION) {
             LogStream.exportLogs(fileChooser.getSelectedFile().toPath());
-            viewManager.showMessageDialog(viewManager.getRootFrame(), "Logs successfully exported.");
+            viewManager.showMessageDialog(viewManager.getRootFrame(), "Logs successfully exported.", ACTION_NAME,
+                    JOptionPane.INFORMATION_MESSAGE);
         }
 
     }

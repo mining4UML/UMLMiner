2caf11c44cdb206ee63990acd3561171188481d6
Vito Alessandro Carella <berlinodeveloper@gmail.com>
2023-06-21 23:48:00 +0200

    change build version

--- src/com/uniba/mining/plugin/Config.java
+++ src/com/uniba/mining/plugin/Config.java
@@ -3,7 +3,7 @@ package com.uniba.mining.plugin;
 public class Config {
     public static final String PLUGIN_ID = "UMLMiner";
     public static final String PLUGIN_NAME = "UML Miner";
-    public static final String PLUGIN_VERSION = "Version 1.0 (Build 20230601)";
+    public static final String PLUGIN_VERSION = "Version 1.0 (Build 20230621)";
     public static final String PLUGIN_HOMEPAGE = "www.link.to.site.it";
     private static final String ROOT_PATH = "";
     private static final String ASSETS_PATH = String.join("/", ROOT_PATH, "assets");

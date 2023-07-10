fff0570079485add516ce47bbf49ef6a7e1eb74c
Vito Alessandro Carella <berlinodeveloper@gmail.com>
2023-07-10 13:54:00 +0200

    update Runtime to ProcessBuilder

--- src/com/uniba/mining/actions/ExternalActionController.java
+++ src/com/uniba/mining/actions/ExternalActionController.java
@@ -1,6 +1,6 @@
 package com.uniba.mining.actions;
 
-import com.uniba.mining.dialogs.RumDialogHandler;
+import com.uniba.mining.dialogs.ExternalDialogHandler;
 import com.uniba.mining.utils.Application;
 import com.vp.plugin.ViewManager;
 import com.vp.plugin.action.VPAction;
@@ -12,31 +12,7 @@ public class ExternalActionController implements VPActionController {
 
     @Override
     public void performAction(VPAction vpAction) {
-        // ExternalTool externalTool =
-        // ExternalTool.getExternalTool(vpAction.getActionId());
-        // String externalToolPath = Config.getExternalToolPath(externalTool);
-        // String[] command = ExternalTool.getExecutionCommand(externalToolPath);
-        // ProcessBuilder processBuilder = new ProcessBuilder(command);
-
-        // Application.run(() -> {
-        // try {
-        // File externalToolDirectory = Path.of(externalToolPath).getParent().toFile();
-        // Path externalToolLogPath = externalToolDirectory.toPath().resolve("ext.log");
-        // File externalToolLog = Files.exists(externalToolLogPath) ?
-        // externalToolLogPath.toFile()
-        // : Files.createFile(externalToolLogPath).toFile();
-        // processBuilder.directory(externalToolDirectory);
-        // processBuilder.redirectErrorStream(true);
-        // processBuilder.redirectOutput(externalToolLog);
-        // Process process = processBuilder.start();
-        // process.waitFor();
-        // } catch (IOException | InterruptedException e) {
-        // e.printStackTrace();
-        // Thread.currentThread().interrupt();
-        // }
-        // });
-
-        viewManager.showDialog(new RumDialogHandler());
+        viewManager.showDialog(new ExternalDialogHandler(vpAction.getActionId()));
     }
 
     @Override
